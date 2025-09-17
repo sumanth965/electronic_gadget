66d1759cac66228f4cf90388e4bb1dcc0d194310
Sumanth <sumanthpoojary965@gmail.com>
2025-09-17 19:26:53 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div className="p-6">
+        <h1 className="text-3xl font-bold underline text-center text-blue-600">
+          Hello world!
+        </h1>
+      </div>
+    </Router>
+  );
+}
